1917789ce8a61b6eb8dab42532359c13387c692c
lisi <392710909@qq.com>
2018-07-28 15:05:27 +0800

    1、整合freemarker 2、远程连接mongodb

--- src/test/java/com/storm/test/TestMongo.java
+++ src/test/java/com/storm/test/TestMongo.java
@@ -33,12 +33,12 @@ public class TestMongo {
         try {
             //连接到MongoDB服务 如果是远程连接可以替换“localhost”为服务器所在IP地址  
             //ServerAddress()两个参数分别为 服务器地址 和 端口  
-            ServerAddress serverAddress = new ServerAddress("localhost",27017);  
+            ServerAddress serverAddress = new ServerAddress("182.61.25.239",27017);  
             List<ServerAddress> addrs = new ArrayList<ServerAddress>();  
             addrs.add(serverAddress);  
               
             //MongoCredential.createScramSha1Credential()三个参数分别为 用户名 数据库名称 密码  
-            MongoCredential credential = MongoCredential.createScramSha1Credential("rootusername", "test", "123456".toCharArray());  
+            MongoCredential credential = MongoCredential.createScramSha1Credential("lisi", "test", "123456".toCharArray());  
             List<MongoCredential> credentials = new ArrayList<MongoCredential>();  
             credentials.add(credential);  
             //通过连接认证获取MongoDB连接  
